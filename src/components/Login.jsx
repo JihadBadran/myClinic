11394fad10b0ffccdd1e6152ed66ae21e110a56b
jihadbadran <jihadm_2009@hotmail.com>
2017-06-24 00:17:05 +0300

    Start

---
+++ src/components/Login.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+
+class LoginFormInput extends React.Component{
+    constructor(props){
+        super(props);
+
+        this.state = {text:""};
+
+
+        this.onChange = this.onChange.bind(this);
+    }
+    onChange(e){
+        this.setState({text:e.target.value});
+    }
+    render(){
+        return (<input className="customInput form-control" placeholder={this.props.placeholder} onChange={this.onChange} value={this.state.text} type={this.props.type} />);
+    }
+}
+
+
+class LoginForm extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+
+        return (<div >
+            <LoginFormInput placeholder="Username" type="text" value=""/>
+            <LoginFormInput placeholder="Password" type="password" value=""/>
+            <button className="customInput" onClick={this.props.handler}>Login </button>
+        </div>);
+    }
+}
+
+
+class Login extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {username:"", password:"", loggedIn:false};
+    }
+
+    render(){
+        console.log("from render " + this.state.loggedIn);
+
+        return (<div>
+            <LoginForm  handler={this.props.handler}/>
+        </div>);
+    }
+}
+
+module.exports = Login;
--- src/script.jsx
+++ src/script.jsx
@@ -2,54 +2,52 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 var List = require("./components/List.jsx");
-// import List from './components/List';
+var Login = require("./components/Login.jsx");
+import {Router, Route, Switch} from "react-router";
+import {BrowserRouter} from "react-router-dom"
 
-class Screen extends React.Component {
+class Screen extends React.Component{
     constructor(props){
         super(props);
-        this.state = {items: [], newItemText: ''};
-        this.handleSubmit = this.handleSubmit.bind(this);
-
-        this.onChange = this.onChange.bind(this);
-    }
-    componentDidMount(){
+        this.state = {loggedin:false};
+        this.loginHandler = this.loginHandler.bind(this);
 
     }
+    loginHandler(){
+        console.log(this.state.loggedIn);
 
-    handleSubmit(e) {
-        e.preventDefault();
-        var currentItems = this.state.items;
-        currentItems.push(this.state.newItemText);
-        this.setState({items: currentItems, newItemText: ""});
+        this.setState({loggedIn:true});
     }
 
-    onChange(e) {
-        var currentItems = this.state.items;
-        this.setState({items: currentItems,newItemText: e.target.value});
-    }
-
-    render() {
-        return (
-            <div className="col-lg-3">
-            <div className="panel panel-default">
-                <div className="panel-heading">
-                    <h1>{this.props.title}</h1>
-                </div>
-
-                <div className="panel-body">
-                    <form ref="myRef" onSubmit={(e)=>this.handleSubmit(e)} >
-                        <div className="input-group">
-                            <span className="input-group-addon">Insert:</span>
-                            <input className="form-control" onChange={this.onChange} type="text" value={this.state.newItemText}/>
-                        </div>
-                        <List ingredients={this.state.items} />
-                    </form>
-                </div>
-
-            </div>
-            </div>
-        );
+    render(){
+        if(!this.state.loggedIn)
+            return (<div className="centerInPage col-lg-4 col-sm-6 col-md-6 col-xs-12" >
+                <Login loggedin={false} handler={this.loginHandler}/>
+            </div>)
+        else{
+            return (<div>
+                <h1>Welcome Abroad!!</h1>
+            </div>);
+        }
     }
 }
 
-ReactDOM.render(<Screen title="Ingredients"/>, document.getElementById("render"));
+class Screen2 extends React.Component{
+    render(){
+
+        return (<div>
+            <h1>Welcome Abroad!!</h1>
+        </div>);
+
+    }
+}
+<Switch>
+    <Route exact path='/' component={Screen}/>
+    <Route path='/roster' component={Screen2}/>
+</Switch>
+
+ReactDOM.render(
+    <BrowserRouter>
+        <Route path="/" component={Screen}/>
+    </BrowserRouter>
+    , document.getElementById("render"));
