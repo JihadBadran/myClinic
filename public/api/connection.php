e9154558eaac04e014f8316fcdeb80875681c576
jihadbadran <jihadm_2009@hotmail.com>
2017-06-26 16:42:11 +0300

    API Start

--- public/api/connection.php
+++ public/api/connection.php
@@ -2,20 +2,22 @@
 class Database{
 
     // specify your own database credentials
-    private $host = "localhost";
+    private $host = "localhost:3306";
     private $db_name = "clinic_management";
     private $username = "root";
     private $password = "1234567890";
+    private $socket = "/tmp/mysql.sock";
     public $conn;
 
     // get the database connection
     public function getConnection(){
-
         $this->conn = null;
 
         try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+
+            $this->conn = new PDO("mysql:host=" . $this->host . ";unix_socket=".$this->socket . ";dbname=" . $this->db_name , $this->username, $this->password);
             $this->conn->exec("set names utf8");
+
         }catch(PDOException $exception){
             echo "Connection error: " . $exception->getMessage();
         }
--- public/api/signin.php
+++ public/api/signin.php
@@ -4,12 +4,13 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
 
 // include database and object files
-include_once './connection.php';
-include_once './doctor.php';
+include_once 'connection.php';
+include_once 'doctor.php';
 
 // instantiate database and product object
 $database = new Database();
 $db = $database->getConnection();
+
 
 // initialize object
 $product = new Product($db);
@@ -43,7 +44,7 @@ if($num>0){
         array_push($products_arr["records"], $product_item);
     }
 
-    echo "\n".json_encode($products_arr)."\n";
+    echo json_encode($products_arr)."\n";
 }
 
 else{
